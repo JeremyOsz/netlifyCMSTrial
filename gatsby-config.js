7cf3f49beb973b4ccb2309379ed7b027e375af60
Jeremy <josz1@student.monash.edu>
2019-07-28 19:41:53 +1000

    Initial commit

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,4 +6,20 @@
 
 module.exports = {
   /* Your site config here */
+    plugins: [`gatsby-plugin-netlify-cms`,
+    {
+      resolve: `gatsby-plugin-netlify-cms`,
+      options: {
+        enableIdentityWidget: false,
+      }
+    },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        path: `${__dirname}/src/markdown-pages`,
+        name: `markdown-pages`,
+      },
+    },
+    `gatsby-transformer-remark`,
+  ]
 }
